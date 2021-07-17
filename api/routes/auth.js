13f84dff780407b166a2b83e31ae6256d7d4f375
Ryan Park <ryan.developer99@gmail.com>
2021-07-17 15:17:59 +0900

    add: login routes successfully

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -22,4 +22,19 @@ router.post("/register", async (req, res) => {
 
 // Login
 
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(400).json("Wrong Credentials");
+
+    const validated = await bcrypt.compare(req.body.password, user.password);
+    !validated && res.status(400).json("Wrong Credentials");
+
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
