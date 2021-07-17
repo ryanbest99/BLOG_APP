3cb3e831d970f1735c08265e8ec1067664b2eabc
Ryan Park <ryan.developer99@gmail.com>
2021-07-17 18:20:58 +0900

    add: delete and get router

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const Post = require("../models/Post");
 const bcrypt = require("bcrypt");
 
 // Update
@@ -23,6 +24,37 @@ router.put("/:id", async (req, res) => {
     }
   } else {
     res.status(401).json("You can update only your account!");
+  }
+});
+
+// Delete
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      try {
+        await Post.deleteMany({ username: user.username });
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted");
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } catch (err) {
+      res.status(404).json("User not found!");
+    }
+  } else {
+    res.status(401).json("You can delete only your account!");
+  }
+});
+
+// Get User
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
