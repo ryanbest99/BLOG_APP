c581c29257f48e08a9bedbff0ba9843d1a640f2a
Ryan Park <ryan.developer99@gmail.com>
2021-07-19 11:50:10 +0900

    add: navbar

---
+++ client/src/components/Navbar/index2.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Index2 = () => {
+  return (
+    <div>
+      <h1>NavBar2</h1>
+    </div>
+  );
+};
+
+export default Index2;
